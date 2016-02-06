7f7a4df00235aa25559050e960f323c54fec4d93
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-02-06 15:59:39 +0100

    travis with build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DNXVERSION="1.0.0-rc1-update1"
+DOWNLOAD_CAKE_BUILD_BOOTSTRAP="curl -Lsfo buildcake.sh http://cakebuild.net/bootstrapper/linux"
+
+if [ ! -f ~/.dnx/dnvm/dnvm.sh ] 
+then
+echo "Downloading dnvm"
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+else
+echo "Using installed dnvm.sh script"
+source ~/.dnx/dnvm/dnvm.sh
+fi
+
+dnvm install $DNXVERSION -a x64 -r coreclr
+dnvm install $DNXVERSION -a x64 -r mono -alias default
+
+dnvm use default
+
+dnu restore
+dnu build --configuration release src/NLog.Framework.logging/project.json
