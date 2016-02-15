a1a3e7d25e1c4d057d063d23fc52a7ebcca12cd9
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-02-15 21:17:38 +0100

    Update example for new packages

--- examples/aspnet5application/src/aspnet5application/Startup.cs
+++ examples/aspnet5application/src/aspnet5application/Startup.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NLog.Framework.Logging;
+using NLog.Extensions.Logging;
 
 namespace aspnet5application
 {
