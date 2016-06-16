6c98343fb4963d37d483b0d588fc7ba33877e0cc
Daan Le Duc <daanleduc@hotmail.com>
2016-06-16 08:12:55 +0200

    Add the actual properties of EventId as the ToString method is not implemented and will return the class name.

--- src/NLog.Extensions.Logging/NLogLogger.cs
+++ src/NLog.Extensions.Logging/NLogLogger.cs
@@ -32,6 +32,8 @@ namespace NLog.Extensions.Logging
                     //message arguments are not needed as it is already checked that the loglevel is enabled.
                     var eventInfo = LogEventInfo.Create(nLogLogLevel, _logger.Name, message);
                     eventInfo.Exception = exception;
+                    eventInfo.Properties["EventId.Id"] = eventId.Id;
+                    eventInfo.Properties["EventId.Name"] = eventId.Name;
                     eventInfo.Properties["EventId"] = eventId;
                     _logger.Log(eventInfo);
                 }
