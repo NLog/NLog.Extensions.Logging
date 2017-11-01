4e40753e786bbfdd5dbd92a23fe6a33293b1ff0f
Sébastien Lucas <sebastien@slucas.fr>
2017-11-01 12:37:34 +0100

    Update NetCore2 ConsoleExample to include the minimumlevel.
    Also add a mandatory dep

--- examples/NetCore2/ConsoleExample/Program.cs
+++ examples/NetCore2/ConsoleExample/Program.cs
@@ -28,6 +28,7 @@ namespace ConsoleExample
 
             services.AddSingleton<ILoggerFactory, LoggerFactory>();
             services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+            services.AddLogging((builder) => builder.SetMinimumLevel(LogLevel.Trace));
 
             var serviceProvider = services.BuildServiceProvider();
 
