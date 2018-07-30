681df9481ca62ad012dfd02413809d69e72ac8a7
Bryan Gonzalez <bgonza868@gmail.com>
2018-07-29 23:26:23 -0400

    Implement UseNLog for netcore 2.1+ Generic Host as distinct assembly. Depends on NLog.Extensions.Logging - needs unit tests.

---
+++ src/NLog.Extensions.Hosting/Extensions/Hosting/ConfigureExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Config;
+using NLog.Extensions.Logging;
+
+namespace NLog.Extensions.Hosting
+{
+    /// <summary>
+    /// Helpers for IHostbuilder, netcore 2.1
+    /// </summary>
+    public static class ConfigureExtensions
+    {
+        /// <summary>
+        /// Enable and configure NLog as a logging provider for buildable generic host (.NET Core 2.1+).
+        /// Can be used in discrete containers as well.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns>IHostBuilder for chaining</returns>
+        public static IHostBuilder UseNLog(this IHostBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            return builder.UseNLog(new NLogProviderOptions
+            {
+                EventIdSeparator = "_",
+                IgnoreEmptyEventId = true,
+                CaptureMessageTemplates = true,
+                CaptureMessageProperties = true,
+                ParseMessageTemplates = false
+            });
+        }
+        
+        /// <summary>
+        /// Enable and configure NLog as a logging provider for buildable generic host (.NET Core 2.1+).
+        /// Can be used in discrete containers as well. 
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="options">NLogProviderOptions object to configure NLog behavior</param>
+        /// <returns>IHostBuilder for chaining</returns>
+        public static IHostBuilder UseNLog(this IHostBuilder builder, NLogProviderOptions options)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.ConfigureServices((hostContext, services) =>
+            {
+                ConfigurationItemFactory.Default.RegisterItemsFromAssembly(typeof(ConfigureExtensions).GetTypeInfo()
+                    .Assembly);
+
+                using (var factory = new LoggerFactory())
+                {
+                    services.AddSingleton(factory.AddNLog(options));
+                }
+            });
+
+            return builder;
+        }
+    }
+}
