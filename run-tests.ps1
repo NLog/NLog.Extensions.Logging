dd07e0d13d0858154120411607d383ba955fe946
Bryan Gonzalez <bgonza868@gmail.com>
2018-07-29 23:41:25 -0400

    Fix travis and appveyor - whoops

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,5 +1,5 @@
-dotnet restore test/NLog.Extensions.Logging.Tests.csproj -v minimal
-dotnet build test/NLog.Extensions.Logging.Tests.csproj  --configuration release -v minimal
-dotnet test test/NLog.Extensions.Logging.Tests.csproj  --configuration release
+dotnet restore test/NLog.Extensions.Tests/NLog.Extensions.Logging.Tests.csproj -v minimal
+dotnet build test/NLog.Extensions.Tests/NLog.Extensions.Logging.Tests.csproj  --configuration release -v minimal
+dotnet test test/NLog.Extensions.Tests/NLog.Extensions.Logging.Tests.csproj  --configuration release
 
 exit $LASTEXITCODE
