ab610bad68dcc11e95c9efe3eb10709e879b69e3
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-06-08 23:39:37 +0200

    Extra tests MicrosoftILogger

--- test/NLog.Extensions.Logging.Tests/MicrosoftILoggerTargetTests.cs
+++ test/NLog.Extensions.Logging.Tests/MicrosoftILoggerTargetTests.cs
@@ -59,7 +59,7 @@ namespace NLog.Extensions.Logging.Tests
             // Arrange
             var (logger, mock) = CreateLoggerMock(out var target);
             target.ContextProperties.Add(new Targets.TargetPropertyWithContext
-                { Name = "ThreadId", Layout = "${threadid}" }
+            { Name = "ThreadId", Layout = "${threadid}" }
             );
 
             // Act
@@ -75,6 +75,114 @@ namespace NLog.Extensions.Logging.Tests
             Assert.Equal("Hello {Planet}", mock.LastLogProperties[2].Value);
         }
 
+
+        [Fact]
+        public void LogWithEventIdTest()
+        {
+            // Arrange
+            var (logger, mock) = CreateLoggerMock(out var target);
+            target.EventId = "${event-properties:item1}";
+
+            // Act
+            logger.WithProperty("item1", 123).Info("Hello there");
+
+            // Assert
+            Assert.Equal("Hello there", mock.LastLogMessage);
+            Assert.Equal(123, mock.LastLogEventId.Id);
+            Assert.Null(mock.LastLogEventId.Name);
+
+            Assert.Equal(2, mock.LastLogProperties.Count);
+            Assert.Equal("item1", mock.LastLogProperties[0].Key);
+            Assert.Equal(123, mock.LastLogProperties[0].Value);
+            Assert.Equal("{OriginalFormat}", mock.LastLogProperties[1].Key);
+            Assert.Equal("Hello there", mock.LastLogProperties[1].Value);
+        }
+
+        [Fact]
+        public void LogWithEventNameAndIdTest()
+        {
+            // Arrange
+            var (logger, mock) = CreateLoggerMock(out var target);
+            target.EventId = "123";
+            target.EventName = "event1";
+
+            // Act
+            logger.WithProperty("item1", 123).Info("Hello there");
+
+            // Assert
+            Assert.Equal("Hello there", mock.LastLogMessage);
+            Assert.Equal(123, mock.LastLogEventId.Id);
+            Assert.Equal("event1", mock.LastLogEventId.Name);
+
+            Assert.Equal(2, mock.LastLogProperties.Count);
+            Assert.Equal("item1", mock.LastLogProperties[0].Key);
+            Assert.Equal(123, mock.LastLogProperties[0].Value);
+            Assert.Equal("{OriginalFormat}", mock.LastLogProperties[1].Key);
+            Assert.Equal("Hello there", mock.LastLogProperties[1].Value);
+        }
+
+        [Fact]
+        public void IncludeEmptyMdc()
+        {
+            // Arrange
+            var (logger, mock) = CreateLoggerMock(out var target);
+            target.IncludeMdc = true;
+
+            // Act
+            logger.Info("Hello there");
+
+            // Assert
+            Assert.Equal("Hello there", mock.LastLogMessage);
+            Assert.Equal(1, mock.LastLogProperties.Count);
+            Assert.Equal("{OriginalFormat}", mock.LastLogProperties[0].Key);
+            Assert.Equal("Hello there", mock.LastLogProperties[0].Value);
+        }
+
+        [Theory]
+        [InlineData("trace", "Trace")]
+        [InlineData("debug", "Debug")]
+        [InlineData("info", "Information")]
+        [InlineData("warn", "Warning")]
+        [InlineData("error", "Error")]
+        [InlineData("fatal", "Critical")]
+        public void TestOnLogLevel(string levelText, string expectedILoggerLogLevelText)
+        {
+            // Arrange
+            var logLevel = LogLevel.FromString(levelText);
+            var success = Enum.TryParse<Microsoft.Extensions.Logging.LogLevel>(expectedILoggerLogLevelText, out var expectedLogLevel);
+            if (!success)
+            {
+                throw new ArgumentException($"Invalid log level '{expectedILoggerLogLevelText}'", nameof(expectedILoggerLogLevelText));
+            }
+
+            var (logger, mock) = CreateLoggerMock();
+            mock.EnableAllLevels = true;
+
+            // Act
+            logger.Log(logLevel, "message1");
+
+            // Assert
+            Assert.Equal("message1", mock.LastLogMessage);
+            Assert.Equal(expectedLogLevel, mock.LastLogLevel);
+        }
+        [Fact]
+        public void LogWitException()
+        {
+            // Arrange
+            var (logger, mock) = CreateLoggerMock();
+            var ex = new ArgumentException("a is not b", nameof(mock));
+
+            // Act
+            logger.Info(ex, "Hello there");
+
+            // Assert
+            Assert.Equal("Hello there", mock.LastLogMessage);
+            Assert.Equal(ex, mock.LastLogException);
+            Assert.Equal(1, mock.LastLogProperties.Count);
+            Assert.Equal("{OriginalFormat}", mock.LastLogProperties[0].Key);
+            Assert.Equal("Hello there", mock.LastLogProperties[0].Value);
+        }
+
         private static (Logger, LoggerMock) CreateLoggerMock()
         {
             return CreateLoggerMock(out _);
@@ -82,8 +190,8 @@ namespace NLog.Extensions.Logging.Tests
 
         private static (Logger, LoggerMock) CreateLoggerMock(out MicrosoftILoggerTarget target)
         {
-            var logFactory = new NLog.LogFactory();
-            var logConfig = new NLog.Config.LoggingConfiguration();
+            var logFactory = new LogFactory();
+            var logConfig = new Config.LoggingConfiguration();
             var loggerMock = new LoggerMock();
             target = new MicrosoftILoggerTarget(loggerMock) { Layout = "${message}" };
             logConfig.AddRuleForAllLevels(target);
@@ -98,7 +206,8 @@ namespace NLog.Extensions.Logging.Tests
             public string LastLogMessage;
             public Exception LastLogException;
             public IList<KeyValuePair<string, object>> LastLogProperties;
-            public Microsoft.Extensions.Logging.EventId LastLogEventId;
+            public EventId LastLogEventId;
+            public bool EnableAllLevels;
 
             public IDisposable BeginScope<TState>(TState state)
             {
@@ -107,6 +216,11 @@ namespace NLog.Extensions.Logging.Tests
 
             public bool IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel)
             {
+                if (EnableAllLevels)
+                {
+                    return true;
+                }
+
                 switch (logLevel)
                 {
                     case Microsoft.Extensions.Logging.LogLevel.Trace:
