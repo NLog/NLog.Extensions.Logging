f41548243e404f6f87818ac6a1d492901fd2f2fa
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-10-07 22:28:54 +0200

    Version 1.6 and changelog

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "1.5.4"
+$versionPrefix = "1.6.0"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
