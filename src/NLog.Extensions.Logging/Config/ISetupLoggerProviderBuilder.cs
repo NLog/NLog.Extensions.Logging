628f1d5cfb436c1d875ce81d28609eeef5ce922b
Rolf Kristensen <sweaty1@hotmail.com>
2020-03-18 19:26:12 +0100

    Introduced SetupExtensionLogging extension method for LogFactory.Setup

---
+++ src/NLog.Extensions.Logging/Config/ISetupLoggerProviderBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace NLog.Extensions.Logging
+{
+    /// <summary>
+    /// Interface for fluent setup of LogFactory integration with Microsoft Extension Logging
+    /// </summary>
+    public interface ISetupExtensionLoggingBuilder
+    {
+        /// <summary>
+        /// LogFactory under configuration
+        /// </summary>
+        LogFactory LogFactory { get; }
+    }
+}
---
+++ src/NLog.Extensions.Logging/Config/SetupBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NLog.Config;
+
+namespace NLog.Extensions.Logging
+{
+    /// <summary>
+    /// Extension methods to setup LogFactory options
+    /// </summary>
+    public static class SetupBuilderExtensions
+    {
+        /// <summary>
+        /// Setup of LogFactory integration with Microsoft Extension Logging
+        /// </summary>
+        public static ISetupBuilder SetupExtensionLogging(this ISetupBuilder setupBuilder, Action<ISetupExtensionLoggingBuilder> extensionsBuilder)
+        {
+            extensionsBuilder(new SetupExtensionLoggingBuilder(setupBuilder.LogFactory));
+            return setupBuilder;
+        }
+
+        private class SetupExtensionLoggingBuilder : ISetupExtensionLoggingBuilder
+        {
+            public SetupExtensionLoggingBuilder(LogFactory logFactory)
+            {
+                LogFactory = logFactory;
+            }
+
+            public LogFactory LogFactory { get; }
+        }
+    }
+}
