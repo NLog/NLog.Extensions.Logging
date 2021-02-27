6ad9d19a6ad26be7359ca81e8cb10f28235145a4
Julian Verdurmen <304NotModified@users.noreply.github.com>
2021-02-27 17:57:23 +0100

    create PR packages

--- build.ps1
+++ build.ps1
@@ -6,23 +6,30 @@ $versionPrefix = "1.7.1"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
-if (-Not $versionSuffix.Equals(""))
-	{ $versionProduct = $versionProduct + "-" + $versionSuffix }
+
+if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
+    $versionPrefix = $versionFile
+    $versionSuffix = "PR" + $env:APPVEYOR_PULL_REQUEST_NUMBER
+}
+
+if (-Not $versionSuffix.Equals("")) { 
+    $versionProduct = $versionProduct + "-" + $versionSuffix 
+}
 
 dotnet restore .\src\NLog.Extensions.Logging\
 if (-Not $LastExitCode -eq 0)
-	{ exit $LastExitCode }
+{ exit $LastExitCode }
 
 dotnet restore .\src\NLog.Extensions.Hosting\
 if (-Not $LastExitCode -eq 0)
-	{ exit $LastExitCode }
+{ exit $LastExitCode }
 
 msbuild /t:Pack .\src\NLog.Extensions.Logging\ /p:targetFrameworks='"net451;net461;netstandard1.3;netstandard1.5;netstandard2.0;netcoreapp3.0;net5.0"' /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ProductVersion=$versionProduct /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
 if (-Not $LastExitCode -eq 0)
-	{ exit $LastExitCode }
+{ exit $LastExitCode }
 
 msbuild /t:Pack .\src\NLog.Extensions.Hosting\ /p:targetFrameworks='"netstandard2.0;netcoreapp3.0;net5.0"' /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ProductVersion=$versionProduct /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
 if (-Not $LastExitCode -eq 0)
-	{ exit $LastExitCode }
+{ exit $LastExitCode }
 
 exit $LastExitCode
