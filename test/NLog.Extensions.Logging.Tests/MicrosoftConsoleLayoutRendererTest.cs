92d29237d5060f3a5b35c1ca5e9d3f04a42e57b7
Rolf Kristensen <sweaty1@hotmail.com>
2021-03-21 20:34:11 +0100

    MicrosoftConsoleLayoutRenderer - For Hosting Lifetime Startup Messages

---
+++ test/NLog.Extensions.Logging.Tests/MicrosoftConsoleLayoutRendererTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NLog.Extensions.Logging.Tests
+{
+    public class MicrosoftConsoleLayoutRendererTest
+    {
+        [Fact]
+        public void MicrosoftConsoleLayoutRenderer_NullEvent()
+        {
+            var layoutRenderer = new MicrosoftConsoleLayoutRenderer();
+            var result = layoutRenderer.Render(LogEventInfo.CreateNullEvent());
+            Assert.Contains("crit: [0]", result);
+        }
+
+        [Fact]
+        public void MicrosoftConsoleLayoutRenderer_ExceptionEvent()
+        {
+            var layoutRenderer = new MicrosoftConsoleLayoutRenderer();
+            var exception = new ArgumentException("Test");
+            var eventId = 42;
+            var result = layoutRenderer.Render(new LogEventInfo(LogLevel.Error, "MyLogger", null, "Alert {EventId_Id}", new object[] { eventId }, exception));
+            Assert.Equal($"fail: MyLogger[{eventId}]{Environment.NewLine}      Alert 42{Environment.NewLine}{exception}", result);
+        }
+    }
+}
