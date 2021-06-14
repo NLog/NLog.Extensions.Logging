613c68e72328ac9bb73ed2609e47649a88cbe8da
Rolf Kristensen <sweaty1@hotmail.com>
2021-06-14 20:13:30 +0200

    Release 1.7.3

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "1.7.2"
+$versionPrefix = "1.7.3"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
