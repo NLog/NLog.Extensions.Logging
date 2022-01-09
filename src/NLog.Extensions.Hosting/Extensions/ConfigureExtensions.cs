bb8aae4d180e97ae7cf8126f1d883774a3076011
Rolf Kristensen <sweaty1@hotmail.com>
2022-01-09 09:44:35 +0100

    Include ContentRootPath when scanning for NLog.config candidate

--- src/NLog.Extensions.Hosting/Extensions/ConfigureExtensions.cs
+++ src/NLog.Extensions.Hosting/Extensions/ConfigureExtensions.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NLog.Config;
 using NLog.Extensions.Logging;
+#if NETSTANDARD2_0
+using IHostEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
+#endif
 
 namespace NLog.Extensions.Hosting
 {
@@ -35,18 +38,18 @@ namespace NLog.Extensions.Hosting
         public static IHostBuilder UseNLog(this IHostBuilder builder, NLogProviderOptions options)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
-            builder.ConfigureServices((builderContext, services) => AddNLogLoggerProvider(services, builderContext.Configuration, options, CreateNLogLoggerProvider));
+            builder.ConfigureServices((builderContext, services) => AddNLogLoggerProvider(services, builderContext.Configuration, builderContext.HostingEnvironment, options, CreateNLogLoggerProvider));
             return builder;
         }
 
-        private static void AddNLogLoggerProvider(IServiceCollection services, IConfiguration configuration, NLogProviderOptions options, Func<IServiceProvider, IConfiguration, NLogProviderOptions, NLogLoggerProvider> factory)
+        private static void AddNLogLoggerProvider(IServiceCollection services, IConfiguration configuration, IHostEnvironment hostEnvironment, NLogProviderOptions options, Func<IServiceProvider, IConfiguration, IHostEnvironment, NLogProviderOptions, NLogLoggerProvider> factory)
         {
             ConfigurationItemFactory.Default.RegisterItemsFromAssembly(typeof(ConfigureExtensions).GetTypeInfo().Assembly);
             LogManager.AddHiddenAssembly(typeof(ConfigureExtensions).GetTypeInfo().Assembly);
-            services.TryAddNLogLoggingProvider((svc, addlogging) => svc.AddLogging(addlogging), configuration, options, factory);
+            services.TryAddNLogLoggingProvider((svc, addlogging) => svc.AddLogging(addlogging), configuration, options, (provider, cfg, opt) => factory(provider, cfg, hostEnvironment, opt));
         }
 
-        private static NLogLoggerProvider CreateNLogLoggerProvider(IServiceProvider serviceProvider, IConfiguration configuration, NLogProviderOptions options)
+        private static NLogLoggerProvider CreateNLogLoggerProvider(IServiceProvider serviceProvider, IConfiguration configuration, IHostEnvironment hostEnvironment, NLogProviderOptions options)
         {
             NLogLoggerProvider provider = new NLogLoggerProvider(options);
 
@@ -63,6 +66,12 @@ namespace NLog.Extensions.Hosting
                 provider.TryLoadConfigurationFromSection(configuration);
             }
 
+            var contentRootPath = hostEnvironment?.ContentRootPath;
+            if (!string.IsNullOrEmpty(contentRootPath))
+            {
+                TryLoadConfigurationFromContentRootPath(provider.LogFactory, contentRootPath);
+            }
+
             return provider;
         }
 
@@ -74,6 +83,33 @@ namespace NLog.Extensions.Hosting
                 ConfigSettingLayoutRenderer.DefaultConfiguration = configuration;
             }
             return configuration;
+        }
+
+        private static void TryLoadConfigurationFromContentRootPath(LogFactory logFactory, string contentRootPath)
+        {
+            logFactory.Setup().LoadConfiguration(config =>
+            {
+                if (config.Configuration.LoggingRules.Count == 0 && config.Configuration.AllTargets.Count == 0)
+                {
+                    var standardPath = System.IO.Path.Combine(contentRootPath, "NLog.config");
+                    if (System.IO.File.Exists(standardPath))
+                    {
+                        config.Configuration = new XmlLoggingConfiguration(standardPath, config.LogFactory);
+                    }
+                    else
+                    {
+                        var lowercasePath = System.IO.Path.Combine(contentRootPath, "nlog.config");
+                        if (System.IO.File.Exists(lowercasePath))
+                        {
+                            config.Configuration = new XmlLoggingConfiguration(lowercasePath, config.LogFactory);
+                        }
+                        else
+                        {
+                            config.Configuration = null;    // Perform default loading
+                        }
+                    }
+                }
+            });
         }
     }
 }
