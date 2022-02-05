48e4c771a6e4a3253bf5f8b5b146804de25fb04a
Michael Mickelson <mickelsonmichael@gmail.com>
2022-02-05 03:59:59 -0700

    MicrosoftConsoleLayoutRenderer - Fixed IndexOutOfRangeException when large EventID (#570)

--- src/NLog.Extensions.Logging/LayoutRenderers/MicrosoftConsoleLayoutRenderer.cs
+++ src/NLog.Extensions.Logging/LayoutRenderers/MicrosoftConsoleLayoutRenderer.cs
@@ -51,7 +51,7 @@ namespace NLog.Extensions.Logging
         {
             if (eventId == 0)
                 return "0";
-            else if (eventId > 0 || eventId < EventIdMapper.Length)
+            else if (eventId > 0 && eventId < EventIdMapper.Length)
                 return EventIdMapper[eventId];
             else
                 return eventId.ToString();
