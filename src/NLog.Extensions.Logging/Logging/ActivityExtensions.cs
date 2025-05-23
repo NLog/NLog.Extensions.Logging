0c8bac57dbdeee1e1b9a9a6c9dba762ee39b7451
Rolf Kristensen <sweaty1@hotmail.com>
2025-05-23 16:27:26 +0200

    ActivityExtensions - Convert empty TraceId into empty string

--- src/NLog.Extensions.Logging/Logging/ActivityExtensions.cs
+++ src/NLog.Extensions.Logging/Logging/ActivityExtensions.cs
@@ -9,34 +9,46 @@ namespace NLog.Extensions.Logging
     /// </summary>
     internal static class ActivityExtensions
     {
+        private static readonly string EmptySpanIdToHexString = default(System.Diagnostics.ActivitySpanId).ToHexString();
+        private static readonly string EmptyTraceIdToHexString = default(System.Diagnostics.ActivityTraceId).ToHexString();
+
         public static string GetSpanId(this Activity activity)
         {
-            return activity.IdFormat switch
-            {
-                ActivityIdFormat.Hierarchical => activity.Id,
-                ActivityIdFormat.W3C => activity.SpanId.ToHexString(),
-                _ => null,
-            } ?? string.Empty;
+            return activity.IdFormat == ActivityIdFormat.W3C ?
+                SpanIdToHexString(activity.SpanId) :
+                activity.Id;
         }
 
         public static string GetTraceId(this Activity activity)
         {
-            return activity.IdFormat switch
-            {
-                ActivityIdFormat.Hierarchical => activity.RootId,
-                ActivityIdFormat.W3C => activity.TraceId.ToHexString(),
-                _ => null,
-            } ?? string.Empty;
+            return activity.IdFormat == ActivityIdFormat.W3C ?
+                TraceIdToHexString(activity.TraceId) :
+                activity.RootId;
         }
 
         public static string GetParentId(this Activity activity)
         {
-            return activity.IdFormat switch
-            {
-                ActivityIdFormat.Hierarchical => activity.ParentId,
-                ActivityIdFormat.W3C => activity.ParentSpanId.ToHexString(),
-                _ => null,
-            } ?? string.Empty;
+            return activity.IdFormat == ActivityIdFormat.W3C ?
+                SpanIdToHexString(activity.ParentSpanId) :
+                activity.ParentId;
+        }
+
+        private static string SpanIdToHexString(ActivitySpanId spanId)
+        {
+            var spanIdString = spanId.ToHexString();
+            if (ReferenceEquals(EmptySpanIdToHexString, spanIdString))
+                return string.Empty;
+            else
+                return spanIdString;
+        }
+
+        private static string TraceIdToHexString(ActivityTraceId traceId)
+        {
+            var traceIdString = traceId.ToHexString();
+            if (ReferenceEquals(EmptyTraceIdToHexString, traceIdString))
+                return string.Empty;
+            else
+                return traceIdString;
         }
     }
 }
