f0193410167995931f49375edc7e7851557c2c0f
Rolf Kristensen <snakefoot@users.noreply.github.com>
2025-07-19 16:08:44 +0200

    Simplify the ConsoleExample (#803)

--- examples/NetCore2/ConsoleExample/Program.cs
+++ examples/NetCore2/ConsoleExample/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NLog;
 using NLog.Extensions.Logging;
 
 namespace ConsoleExample
@@ -11,41 +9,21 @@ namespace ConsoleExample
     {
         private static void Main()
         {
-            var config = new ConfigurationBuilder().Build();
+            // Disposing the ServiceProvider will also flush / dispose / shutdown the NLog Logging Provider
+            using var servicesProvider = new ServiceCollection()
+                .AddTransient<Runner>() // Runner is the custom class
+                .AddLogging(loggingBuilder =>
+                {
+                    // configure Logging with NLog
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddNLog();
+                }).BuildServiceProvider();
 
-            var logger = LogManager.Setup()
-                                   .SetupExtensions(ext => ext.RegisterConfigSettings(config))
-                                   .GetCurrentClassLogger();
+            var runner = servicesProvider.GetRequiredService<Runner>();
+            runner.DoAction("Action1");
 
-            try
-            {
-                using var servicesProvider = new ServiceCollection()
-                    .AddTransient<Runner>() // Runner is the custom class
-                    .AddLogging(loggingBuilder =>
-                    {
-                        // configure Logging with NLog
-                        loggingBuilder.ClearProviders();
-                        loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                        loggingBuilder.AddNLog(config);
-                    }).BuildServiceProvider();
-
-                var runner = servicesProvider.GetRequiredService<Runner>();
-                runner.DoAction("Action1");
-
-                Console.WriteLine("Press ANY key to exit");
-                Console.ReadKey();
-            }
-            catch (Exception ex)
-            {
-                // NLog: catch any exception and log it.
-                logger.Error(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                LogManager.Shutdown();
-            }
+            Console.WriteLine("Press ANY key to exit");
+            Console.ReadKey();
         }
     }
 
