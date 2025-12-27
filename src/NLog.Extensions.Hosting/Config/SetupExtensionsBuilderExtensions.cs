c85985dc6a410b4f3a8d4208457aac964f7cd253
Rolf Kristensen <sweaty1@hotmail.com>
2025-12-27 21:04:40 +0100

    Introduced WriteToJsonConsole for fluent API to match AddJsonConsole

--- src/NLog.Extensions.Hosting/Config/SetupExtensionsBuilderExtensions.cs
+++ src/NLog.Extensions.Hosting/Config/SetupExtensionsBuilderExtensions.cs
@@ -19,5 +19,27 @@ namespace NLog.Extensions.Hosting
                         .RegisterLayoutRenderer<HostRootDirLayoutRenderer>("host-rootdir")
                         .RegisterLayoutRenderer<HostEnvironmentLayoutRenderer>("host-environment");
         }
+
+        /// <summary>
+        /// Write to <see cref="NLog.Targets.FileTarget"/> using <see cref="MicrosoftConsoleJsonLayout"/>
+        /// </summary>
+        public static ISetupConfigurationTargetBuilder WriteToJsonFile(this ISetupConfigurationTargetBuilder configBuilder, NLog.Layouts.Layout fileName, bool includeScopes = false)
+        {
+            var jsonLayout = new MicrosoftConsoleJsonLayout();
+            if (includeScopes)
+                jsonLayout.IncludeScopes = includeScopes;
+            return configBuilder.WriteToFile(fileName, jsonLayout);
+        }
+
+        /// <summary>
+        /// Write to <see cref="NLog.Targets.ConsoleTarget"/> using <see cref="MicrosoftConsoleJsonLayout"/> (similar to Microsoft AddJsonConsole)
+        /// </summary>
+        public static ISetupConfigurationTargetBuilder WriteToJsonConsole(this ISetupConfigurationTargetBuilder configBuilder, bool includeScopes = false)
+        {
+            var jsonLayout = new MicrosoftConsoleJsonLayout();
+            if (includeScopes)
+                jsonLayout.IncludeScopes = includeScopes;
+            return configBuilder.WriteToConsole(jsonLayout);
+        }
     }
 }
